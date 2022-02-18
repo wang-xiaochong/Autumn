2b97c7d5acda8b4b1c0a9f24d1457165ac043f5f
xiaoshuai <1378334435@qq.com>
2022-02-18 18:46:32 +0800

    webpack 打包四种资源类型

--- 开发环境/webpack babel/test-pack2/src/index.js
+++ 开发环境/webpack babel/test-pack2/src/index.js
@@ -1,4 +1,26 @@
 
 import hello from './hello.js'
+import imgsrc from './assets/08.jpg'
+import imgsrc2 from './assets/06.svg'
+import txt from './assets/test.txt'
+import imgsrc3 from './assets/07.png'
+
 
 hello()
+
+const img = document.createElement('img')
+img.src = imgsrc
+document.body.appendChild(img)
+
+const img2 = document.createElement('img')
+img2.src = imgsrc2
+document.body.appendChild(img2)
+
+const div = document.createElement('div')
+div.style.cssText = 'width:200px;height:200px;background:yellow'
+div.textContent = txt
+document.body.appendChild(div)
+
+const img3 = document.createElement('img')
+img3.src = imgsrc3
+document.body.appendChild(img3)
--- 开发环境/webpack babel/test-pack2/webpack.config.js
+++ 开发环境/webpack babel/test-pack2/webpack.config.js
@@ -11,7 +11,9 @@ module.exports = {
     output: {
         filename: 'bundle.js',  // 新生成的文件名称
         path: path.resolve(__dirname, './dist'),    //新生成的打包文件位置 
-        clean: true //清理上一次打包生成的文件
+        clean: true, //清理上一次打包生成的文件
+
+        assetModuleFilename: 'images/[contenthash][ext]' //自定义图片打包后图片的输出位置
     },
     mode: 'development', //开发环境
 
@@ -29,7 +31,43 @@ module.exports = {
 
     devServer: {
         static: './dist'
+    },
+
+    // 资源模块
+    module: {
+        // 规则 数组 配置很多的对象加载不同的文件
+        rules: [
+            // 加载四种资源类型
+            {
+                test: /\.jpg$/,         //定义加载的文件的类型
+                type: 'asset/resource',   //资源类型
+                generator: {
+                    filename: 'images/[contenthash][ext]' //优先级高于上面的out
+                }
+            },
+            {
+                test: /\.svg$/,
+                type: 'asset/inline'
+            },
+            {
+                test: /\.txt$/,
+                type: 'asset/source'
+            },
+            {
+                test: /\.png$/,
+                type: 'asset', //根据大小自动选择是resource还是inline
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 4 * 1024 * 1024 //默认大小为8kb 该设置为4Mb 超过后dist会生成文件
+                    }
+                }
+            }
+
+        ]
+
     }
+
+
 }
 
 
